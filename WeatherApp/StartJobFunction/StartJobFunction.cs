9ead69261d99f58ce237662b5498b53e090b8d4f
LarsHartendorp <690720@student.inholland.nl>
2024-11-06 11:57:02 +0100

    update

---
+++ WeatherApp/StartJobFunction/StartJobFunction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartJobFunction;
+
+public class StartJobFunction
+{
+    private readonly ILogger<StartJobFunction> _logger;
+
+    public StartJobFunction(ILogger<StartJobFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("StartJobFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+        
+    }
+
+}
